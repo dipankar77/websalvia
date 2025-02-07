145506fce7c709a0ec1aa32b779b157bb155acc9
dipankar77 <dipankar77@gmail.com>
2025-02-07 14:16:04 +0530

    Home Page Change

--- inc/home.php
+++ inc/home.php
@@ -10,7 +10,7 @@
                 </div>
             </div> -->
             <div class="about_title">
-                <h3>Welcome to Websalvia Solutions1234!</h3>
+                <h3>Welcome to Websalvia Solutions!</h3>
                 <span>Transforming Ideas into Powerful Solutions</span>
             </div>
             <div class="about_text">
